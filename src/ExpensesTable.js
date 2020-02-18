00433874408f19fec812ad69136d17926255ee33
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2020-02-18 15:19:25 +0000

    Small changes

---
+++ src/ExpensesTable.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+// class ExpenseTable extends React.Component {
+//   render() {
+//     return (
+//       DataView
+//     );
+//   }
+// }
+
+// export default BudgetPie;
